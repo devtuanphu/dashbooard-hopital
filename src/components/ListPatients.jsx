95f550387736811654392798f85d3eca8b90ac3a
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2025-03-03 20:19:36 -0600

    modified

--- src/components/ListPatients.jsx
+++ src/components/ListPatients.jsx
@@ -7,23 +7,23 @@ const ListPatients = ({ patients }) => {
   const [selectedPatient, setSelectedPatient] = useState(null);
 
   return (
-    <div className="bg-white rounded-[16px] shadow-md w-full">
+    <div className=" bg-white rounded-[16px] shadow-md w-full">
       {/* Header section with title and search icon */}
-      <div className="p-[20px] bg-white z-10 rounded-[16px] ">
-        <div className="flex justify-between items-center">
+      <div className=" bg-white z-10 rounded-[16px] p-[20px]">
+        <div className="flex justify-between items-center ">
           <h2 className="font-bold text-[24px]">Patients</h2>
           <img src={IconSearch} width="18px" height="18px" alt="Search Icon" />
         </div>
       </div>
 
       {/* Scrollable patient list container with padding-bottom */}
-      <div className="xl:max-h-[930px] overflow-y-auto custom-scrollbar mb-[30px] mr-[5px]">
+      <div className="xl:max-h-[930px] overflow-y-auto custom-scrollbar mb-[30px] mr-[5px]  ">
         <List
           itemLayout="horizontal"
           dataSource={patients}
           renderItem={(patient) => (
             <List.Item
-              className={`px-[20px] py-2 flex justify-between items-center cursor-pointer !border-none custom-font-family
+              className={`cursor-pointer !border-none custom-font-family
               ${
                 selectedPatient === patient.name ||
                 patient.name === "Jessica Taylor"
@@ -36,6 +36,7 @@ const ListPatients = ({ patients }) => {
                 avatar={<Avatar src={patient.profile_picture} size={48} />}
                 title={<span className="font-bold">{patient.name}</span>}
                 description={`${patient.gender}, ${patient.age}`}
+                className="pl-[7px]"
               />
               <div>
                 <img
