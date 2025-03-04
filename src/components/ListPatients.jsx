bfc04bc2845b45fa240de6d2b6f7e2684c1efc00
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2025-03-03 19:31:14 -0600

    modified scrollbar and height

--- src/components/ListPatients.jsx
+++ src/components/ListPatients.jsx
@@ -4,43 +4,39 @@ import IconSearch from "../assets/image/search_FILL0_wght300_GRAD0_opsz24.svg";
 import MoreIcon from "../assets/image/more_horiz_FILL0_wght300_GRAD0_opsz24.svg";
 
 const ListPatients = ({ patients }) => {
-  // State to keep track of the selected patient
   const [selectedPatient, setSelectedPatient] = useState(null);
 
   return (
-    <div className="p-[20px] bg-white rounded-[16px] shadow-md w-full">
+    <div className="bg-white rounded-[16px] shadow-md w-full">
       {/* Header section with title and search icon */}
-      <div className="sticky top-[-25px] bg-white z-10 p-3">
+      <div className="p-[20px] bg-white z-10 rounded-[16px] ">
         <div className="flex justify-between items-center">
           <h2 className="font-bold text-[24px]">Patients</h2>
           <img src={IconSearch} width="18px" height="18px" alt="Search Icon" />
         </div>
       </div>
 
-      {/* Scrollable patient list container */}
-      <div className="xl:max-h-[930px] overflow-y-auto custom-scrollbar mr-[-16px]">
+      {/* Scrollable patient list container with padding-bottom */}
+      <div className="xl:max-h-[930px] overflow-y-auto custom-scrollbar mb-[30px] mr-[5px]">
         <List
           itemLayout="horizontal"
-          dataSource={patients} // Data source for patient list
+          dataSource={patients}
           renderItem={(patient) => (
             <List.Item
-              className={`px-[20px] py-2 flex justify-between items-center cursor-pointer !border-none custom-font-family gf
+              className={`px-[20px] py-2 flex justify-between items-center cursor-pointer !border-none custom-font-family
               ${
                 selectedPatient === patient.name ||
                 patient.name === "Jessica Taylor"
                   ? "bg-[#D8FCF7]" // Highlight selected patient and "Jessica Taylor"
                   : "hover:bg-[#D8FCF7]" // Apply hover effect for other patients
               }`}
-              onClick={() => setSelectedPatient(patient.name)} // Set selected patient on click
+              onClick={() => setSelectedPatient(patient.name)}
             >
-              {/* Patient Avatar, Name, and Details */}
               <List.Item.Meta
                 avatar={<Avatar src={patient.profile_picture} size={48} />}
                 title={<span className="font-bold">{patient.name}</span>}
                 description={`${patient.gender}, ${patient.age}`}
               />
-
-              {/* More options icon */}
               <div>
                 <img
                   src={MoreIcon}
